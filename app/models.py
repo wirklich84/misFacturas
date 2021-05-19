3a3da73b33528f1a645f25a333eb5128511cb3e4
Erik de la cruz <eriks84@hotmail.com>
2021-05-18 23:39:14 -0500

    uploads

--- app/models.py
+++ app/models.py
@@ -6,8 +6,14 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(200), unique=True)
     password = db.Column(db.String(200))
     nombre = db.Column(db.String(300))
+    cer_file = db.Column(db.String(100), nullable=True)
+    key_file = db.Column(db.String(100), nullable=True)
+    pass_fiel = db.Column(db.String(100), nullable=True)
 
-    def __init__(self, email, passsword, nombre):
+    def __init__(self, email, passsword, nombre, cer_file, key_file, pass_fiel):
         self.email = email
         self.password = passsword
         self.nombre = nombre
+        self.cer_file = cer_file
+        self.key_file = key_file
+        self.pass_fiel = pass_fiel
